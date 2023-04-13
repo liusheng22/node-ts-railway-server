f41cc92e53f328fd550fb80d584abe1f2f3cad26
liusheng <liusheng@wangxiaobao.com>
2023-04-13 14:34:20 +0800

    ✨ feat: add koa

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log(process.env.PORT)
+import Koa from "koa"
+import bodyParser from "koa-bodyparser"
+import Router from "koa-router"
+
+const port = 4000
+const app = new Koa()
+const router = new Router()
+
+router.get("/", async (ctx, next) => {
+	ctx.body = "Hello World!"
+})
+
+router.get("/user", async (ctx, next) => {
+	ctx.body = "Hello User!"
+})
+
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen({ port }, () => {
+	console.log(`🚀 Server ready at http://localhost:${port}`)
+})
